cfd55f42278f36414cf5b90363e11a65854830d0
333Nikita333 <nikitosfox333@gmail.com>
2023-04-25 13:55:45 +0300

    deleting redundant comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import GlobalStyle from 'GlobalStyles';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
@@ -13,8 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
